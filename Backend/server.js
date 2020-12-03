ee4aac9d73bb1a2bbe5751cf5542c144d3684f98
llee1990 <leonlee123@gmail.com>
2020-12-02 21:47:52 -0800

    added swagger UI to backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,53 @@
 const express = require("express");
 const router = require('./routes/router.js');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const cors = require("cors");
 
-const app = express();
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "eBookServices API",
+      version: "0.1.0",
+      description:
+        "This is a CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+      {
+        url: "https://stark-wildwood-96064.herokuapp.com",
+      },
+    ],
+    components: {        
+      "securitySchemes": {
+        "bearerAuth": {
+          "type": "http",
+          "scheme": "bearer",
+          "bearerFormat": "JWT"
+        }
+      }
+    },
+  },
+  apis: ["./routes/router.js"],
+};
 
-// var corsOptions = {
-//   origin: "http://localhost:8081"
-// };
+const app = express();
+const specs = swaggerJsdoc(options);
 
 app.use(cors());
+
+app.use(
+  "/doc",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 
 // parse requests of content-type - application/json
 app.use(express.json({ limit: '5mb' }));
